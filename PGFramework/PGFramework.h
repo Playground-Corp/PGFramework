4a8da28b84a81a5616ecf9f2e94a916ce44b57bf
Hiroki Umatani <h.umatani@playground.style>
2019-03-11 19:33:21 +0900

    add ignore file

---
+++ PGFramework/PGFramework.h
@@ -0,0 +1,19 @@
+//
+//  PGFramework.h
+//  PGFramework
+//
+//  Created by Hiroki Umatani on 2019/03/11.
+//  Copyright © 2019 Playground. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PGFramework.
+FOUNDATION_EXPORT double PGFrameworkVersionNumber;
+
+//! Project version string for PGFramework.
+FOUNDATION_EXPORT const unsigned char PGFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PGFramework/PublicHeader.h>
+
+
