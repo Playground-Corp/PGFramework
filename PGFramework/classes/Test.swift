fb518f229848f10d5ca088924a2fda2ba62ae9b6
Hiroki Umatani <h.umatani@playground.style>
2019-03-11 20:13:31 +0900

    modify pod spec

---
+++ PGFramework/classes/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  PGFramework
+//
+//  Created by Hiroki Umatani on 2019/03/11.
+//  Copyright © 2019 Playground. All rights reserved.
+//
+
+import UIKit
+
+class Test: UIView {
+
+}
